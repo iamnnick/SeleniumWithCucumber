09668d5de437dbc225c6989a8bf2e1ef8b9516a9
Jain <sarveshkumar.jain@cgi.com>
2019-05-09 21:58:55 +0530

    Util update

--- src/main/java/Utilities/Utilities.java
+++ src/main/java/Utilities/Utilities.java
@@ -1,9 +1,31 @@
 package Utilities;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Utilities {
 	
+	WebDriver driver=new ChromeDriver();
 	public void takeScreenShot() {
-		
+		// Take screenshot and store as a file format
+		WebDriver driver=new ChromeDriver();
+		File src= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		try {
+		 // now copy the  screenshot to desired location using copyFile //method
+		FileUtils.copyFile(src, new File("C:\\Selenium Tools\\error.png"));
+		}
+		 
+		catch (IOException e)
+		 {
+		  System.out.println(e.getMessage());
+		 
+		 }
 	}
 	
 	public void readFromExcel() {
